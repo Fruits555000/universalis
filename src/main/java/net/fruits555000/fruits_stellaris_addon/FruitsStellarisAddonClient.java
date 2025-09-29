67af7c0672ce20aa1651684e6723f418f201b320
Fruits555000 <robert.hirst3636@gmail.com>
2025-09-29 17:53:58 +1000

    setup

---
+++ src/main/java/net/fruits555000/fruits_stellaris_addon/FruitsStellarisAddonClient.java
@@ -0,0 +1,10 @@
+package net.fruits555000.fruits_stellaris_addon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FruitsStellarisAddonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
